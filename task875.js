b8c1bcd8ffae6260c4923443bc9c968aca3459b7
warmboar <warmblackodor@gmail.com>
2024-01-04 01:30:26 +0300

    sad

--- task875.js
+++ task875.js
@@ -18,5 +18,5 @@ function uniqueAndSorted(arr) {
  
   let numbers = [5, 3, 2, 4, 1, 2, 3, 4, 5];
   let uniqueNumbers = uniqueAndSorted(numbers);
-  console.log(uniqueNumbers); // [1, 2, 3, 4, 5]
+  console.log(uniqueNumbers); 
   
